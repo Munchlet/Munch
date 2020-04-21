a4a8fa6692f04be4122dcce58d072fc3709b902d
ShaunLWM <shaunlimweimin@gmail.com>
2020-04-21 23:38:59 +0800

    Added bottom padding to GalleryListHeader

--- src/components/GalleryList/GalleryListHeader.jsx
+++ src/components/GalleryList/GalleryListHeader.jsx
@@ -2,6 +2,11 @@ import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 const useStyles = makeStyles((theme) => ({
+	headerDiv: {
+		position: "relative",
+		width: "100%",
+		marginBottom: "1.5rem",
+	},
 	headerSeeAll: {
 		top: "0.7rem",
 		right: 0,
@@ -19,7 +24,7 @@ export default function GalleryListHeader() {
 	const classes = useStyles();
 
 	return (
-		<div style={{ position: "relative", width: "100%" }}>
+		<div className={classes.headerDiv}>
 			<Typography variant="h6">What's New?</Typography>
 			<Typography className={classes.headerSubtitle} variant="subtitle2" gutterBottom>
 				Check out what's new
